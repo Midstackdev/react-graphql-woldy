a23b29fb51e6b06d37b60a9874a871e2c354f776
Midstackdev <midstackdev@gmail.com>
2020-11-10 12:19:19 +0000

    Login a user

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,8 +2,17 @@ import User from '../../models/User.js'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import  apollo  from "apollo-server"
+import  { validateRegisterInput, validateLoginInput }  from "../../utils/validators.js"
 
 const { UserInputError } = apollo
+
+function generateToken(user) {
+  return jwt.sign({
+    id: user.id,
+    email: user.email,
+    username: user.username
+  }, process.env.TOKEN_SECRET, { expiresIn: '1h'})
+}
 
 export default {
   Query: {
@@ -18,6 +27,34 @@ export default {
   },
 
   Mutation: {
+    async login(_, {username, password}) {
+      const {errors, valid} = validateLoginInput(username, password)
+      const user = await User.findOne({ username })
+
+      if(!valid) {
+        throw new UserInputError('Wrong credentials', { errors })
+      }
+      
+      if(!user) {
+        errors.general = 'User not found'
+        throw new UserInputError('User not found', { errors })
+      }
+      
+      const match = await bcrypt.compare(password, user.password)
+      if(!match) {
+        errors.general = 'Wrong credentials'
+        throw new UserInputError('Wrong credentials', { errors })
+      }
+
+      const token = generateToken(user)
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token
+      }
+    },
+
     async register(_, 
     {
       registerInput: { username, email, password, confirmPassword }
@@ -25,6 +62,13 @@ export default {
     context, 
     info
     ){
+      // Validate user data
+      const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword)
+      if(!valid) {
+        throw new UserInputError('Errors', {
+          errors
+        })
+      }
       // Make sure user doesnt already exist
       const user = await User.findOne({ username })
       if(user) {
@@ -46,11 +90,7 @@ export default {
 
       const res = await newUser.save()
       
-      const token = jwt.sign({
-        id: res.id,
-        email: res.email,
-        username: res.username
-      }, process.env.TOKEN_SECRET, { expiresIn: '1h'})
+      const token = generateToken(res)
 
       return {
         ...res._doc,
